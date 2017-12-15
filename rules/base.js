6124de3dd7a7192f0b184dcb9c617443d164aa96
David Shim <ssm0123@gmail.com>
2017-12-15 12:28:05 +0900

    require 룰 수정

--- rules/base.js
+++ rules/base.js
@@ -44,8 +44,12 @@ module.exports = {
         'import/extensions': 0, // import할때 확장자 써도 됨
         'import/first': 0, // absolute/relative import 위치 상관 없이
         'import/no-mutable-exports': 0,
+        "import/no-dynamic-require": 0,
+        "import/newline-after-import": 0,
 
         'no-mixed-operators': 0, // 연산자를 연결해서 쓸 수 있도록
-        'jsx-a11y/anchor-is-valid': 0 // anchor 태그를 button 대신 쓸 수 있도록 (IE 스타일 오류 때문)
+        'jsx-a11y/anchor-is-valid': 0, // anchor 태그를 button 대신 쓸 수 있도록 (IE 스타일 오류 때문)
+
+        "no-plusplus": 0
     }
 };
