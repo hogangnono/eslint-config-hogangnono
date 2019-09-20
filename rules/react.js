6e97449ca6ca6a2f4d314fe66adeabeea08280af
David Shim <ssm0123@gmail.com>
2019-09-20 18:48:18 +0900

    jsx-props-no-spreading 끔

--- rules/react.js
+++ rules/react.js
@@ -68,6 +68,7 @@ module.exports = {
         'react/jsx-max-props-per-line': 0,
         'react/self-closing-comp': 0,
         'react/destructuring-assignment': 0,
+        'react/jsx-props-no-spreading': 0,
 
         // 이벤트 핸들러 있는곳에는 role 필요
         'jsx-a11y/anchor-is-valid': 1,
